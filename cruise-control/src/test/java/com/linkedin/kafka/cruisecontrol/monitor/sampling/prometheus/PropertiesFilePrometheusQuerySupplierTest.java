8d0795f25139d31053d90df113af6f8885e758e8
gustavomonarin <gustavomonarin@gmail.com>
2021-05-02 23:05:33 +0200

    wip: first tests

---
+++ cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/monitor/sampling/prometheus/PropertiesFilePrometheusQuerySupplierTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2020 LinkedIn Corp. Licensed under the BSD 2-Clause License (the "License"). See License in the project root for license information.
+ */
+package com.linkedin.kafka.cruisecontrol.monitor.sampling.prometheus;
+
+public class PropertiesFilePrometheusQuerySupplierTest {
+
+  void testDefaultWhenNotPresent() {
+  }
+
+  void testProvidedConfigFileProperty() {
+  }
+
+  //  @Test(expected = ConfigException.class)
+  void testConfigureWithFileNotPresentProperty() {
+
+  }
+
+  void missedQueryies() {
+
+  }
+}
